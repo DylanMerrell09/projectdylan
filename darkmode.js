a249d8c3504eff1728c772fbf30cb54e188aff87
Dylan Merrell <dylan.merrell@duke.edu>
2024-12-18 11:21:17 -0500

    js

---
+++ darkmode.js
@@ -0,0 +1,25 @@
+const darkModeToggle = document.getElementById('dark-mode-toggle');
+
+
+if (localStorage.getItem('theme') === 'dark') {
+    document.body.classList.add('dark-mode');
+    darkModeToggle.textContent = "☀️"; 
+} else {
+    document.body.classList.remove('dark-mode');
+    darkModeToggle.textContent = "🌙"; 
+}
+
+
+darkModeToggle.addEventListener('click', () => {
+    
+    document.body.classList.toggle('dark-mode');
+
+    
+    if (document.body.classList.contains('dark-mode')) {
+        localStorage.setItem('theme', 'dark');
+        darkModeToggle.textContent = "☀️"; 
+    } else {
+        localStorage.setItem('theme', 'light');
+        darkModeToggle.textContent = "🌙"; 
+    }
+});
